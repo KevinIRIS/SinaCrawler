b5c20eefe3156c4dbb39f0824e9e3af658033246
KevinIRIS <kaizhanguestc@gmail.com>
2014-09-27 17:19:48 +0800

    change

--- Main/__init__.py
+++ Main/__init__.py
@@ -4,7 +4,7 @@ from Main.Login import Login
 from Fetcher.Fetcher import Fetcher
 
 username = ''.encode(encoding='utf_8', errors='strict')
-weibo1 = Login("15833127621", "zk19920927")
+weibo1 = Login("", "")
 weibo1.login
 cookie = weibo1.getcookie()
 fetcher = Fetcher(cookie)
